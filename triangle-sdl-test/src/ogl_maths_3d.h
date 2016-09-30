101196b8d6ed7604a8c7f28e65c3b8b60ebbe2b4
Gareth Owen <gareth.w.owen@gmail.com>
2016-09-30 23:56:58 +0100

    Started triangle rotation experiment

---
+++ triangle-sdl-test/src/ogl_maths_3d.h
@@ -0,0 +1,13 @@
+#ifndef OGL_MATHS_H
+#define OGL_MATHS_H
+
+#include <cstdio>
+#include <cmath>
+
+#include <assimp/vector3.h>
+#include <assimp/matrix3x3.h>
+#include <assimp/matrix4x4.h>
+
+
+
+#endif
---
+++ triangle-sdl-test/src/ogl_types.h
@@ -0,0 +1,8 @@
+#ifndef OGLTYPES_H
+#define OGLTYPES_H
+
+typedef unsigned int uint;
+typedef unsigned short ushort;
+typedef unsigned char uchar;
+
+#endif
