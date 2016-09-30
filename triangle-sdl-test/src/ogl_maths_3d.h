fe59b01606736f1fc240f0f34eb0b5615baf66ef
Gareth Owen <gareth.w.owen@gmail.com>
2016-10-01 00:05:51 +0100

    Oh, updates without saving again...

--- triangle-sdl-test/src/ogl_maths_3d.h
+++ triangle-sdl-test/src/ogl_maths_3d.h
@@ -8,6 +8,10 @@
 #include <assimp/matrix3x3.h>
 #include <assimp/matrix4x4.h>
 
+#define ToRadian(x) (float)(((x) * M_PI / 180.0f))
+#define ToDegree(x) (float)(((x) * 180.0f / M_PI))
+
+
 
 
 #endif
